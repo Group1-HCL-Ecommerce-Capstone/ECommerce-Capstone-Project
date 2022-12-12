d1c008f91732e6b0bfca9ddeb182b34563916840
Veronica A Gutierrez Camacho <113631941+veronica-agc@users.noreply.github.com>
2022-12-12 14:58:54 -0500

    Delete Back-End/CapstoneOctVGC-version2 directory

--- Back-End/CapstoneOctVGC-version2/src/main/java/com/capstone/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capstone.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, String>{
-
-}
--- Back-End/CapstoneOctVGC-version2/src/main/java/com/capstone/service/RegistrationService.java
+++
@@ -1,5 +0,0 @@
-package com.capstone.service;
-
-public class RegistrationService {
-
-}
